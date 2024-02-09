35beb0ad2e5bc05b018f62f8ce908d6c1d0965e3
Massimo Biagioli <biagiolimassimo@gmail.com>
2024-02-09 09:06:01 +0100

    Fix swagger title and description

--- fastapi_crud_demo/main.py
+++ fastapi_crud_demo/main.py
@@ -6,7 +6,10 @@ from fastapi_crud_demo.routes import health, team, user
 
 logging.getLogger("passlib").setLevel(logging.ERROR)
 
-app = FastAPI()
+app = FastAPI(
+    title="FastAPI CRUD Demo",
+    description="A simple CRUD demo using FastAPI",
+)
 
 app.include_router(health.router)
 app.include_router(user.router)
